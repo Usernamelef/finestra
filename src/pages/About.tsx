f0e451537bacd29f3998b2bc96b90932777c2c69
Usernamelef <flavio.metrangolo@gmail.com>
2025-07-04 21:10:23 +0200

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -17,6 +17,7 @@ const About = () => {
     '/la-finestra-geneve-equipe-restaurant.jpg',
     '/la-finestra-geneve-plat-specialite.jpg',
     '/la-finestra-geneve-facade-enseigne.jpeg'
+  ];
 
   return (
     <div className="animate-fade-in">

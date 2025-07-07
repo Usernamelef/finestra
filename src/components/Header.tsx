5a07aee906d87a8d126e82fe50f7cacc06d3bf16
Usernamelef <flavio.metrangolo@gmail.com>
2025-07-07 14:36:02 +0200

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,7 +40,7 @@ const Header = () => {
               <img
                 src="/assets/lafinestra-geneve-logo-blanc.png"
                 alt="La Finestra Genève"
-                className="h-16 w-auto"
+                className="h-24 w-auto"
               />
             </Link>
 
